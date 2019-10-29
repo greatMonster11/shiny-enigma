d934701b5295d884c6651c10ad01c467549619a0
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-29 22:46:19 +0700

    add compose for authentication with base

--- src/components/Firebase/context.js
+++ src/components/Firebase/context.js
@@ -2,4 +2,10 @@ import React from "react";
 
 const FirebaseContext = React.createContext(null);
 
+export const withFirebase = Component => props => (
+  <FirebaseContext.Component>
+    {firebase => <Component {...props} firebase={firebase} />}
+  </FirebaseContext.Component>
+);
+
 export default FirebaseContext;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 
-import { Link } from "react-router-dom";
-import { FirebaseContext } from "../Firebase";
+import { Link, withRouter } from "react-router-dom";
+import { compose } from "recompose";
+
+import { withFirebase } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
 
 const SignUpPage = () => (
   <div>
     <h1>SignUp</h1>
-    <FirebaseContext.Consumer>
-      {firebase => <SignUpForm firebase={firebase} />}
-    </FirebaseContext.Consumer>
+    <SignUpForm />
   </div>
 );
 
@@ -21,7 +21,7 @@ const INITIAL_STATE = {
   error: null
 };
 
-class SignUpForm extends Component {
+class SignUpFormBase extends Component {
   constructor(props) {
     super(props);
 
@@ -34,6 +34,7 @@ class SignUpForm extends Component {
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
         this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
         this.setState({ error });
@@ -99,5 +100,9 @@ const SignUpLink = () => (
   </p>
 );
 
+const SignUpForm = compose(
+  withRouter,
+  withFirebase
+)(SignUpFormBase);
 export default SignUpPage;
 export { SignUpForm, SignUpLink };
