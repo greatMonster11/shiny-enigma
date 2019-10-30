dc83b9cc5bf21887af2c4503fbdfdf87eec2b9a1
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-30 23:00:55 +0700

    add authentication for navigation component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -13,6 +13,8 @@ import AdminPage from "../Admin";
 import * as ROUTES from "../../constants/routes";
 import { withFirebase } from "../Firebase";
 
+import { AuthUserContext } from "../Session";
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -36,25 +38,27 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div>
-          <Navigation authUser={this.state.authUser} />
+      <AuthUserContext.Provider value={this.state.authUser}>
+        <Router>
+          <div>
+            <Navigation />
 
-          <hr />
+            <hr />
 
-          <Route exact path={ROUTES.LANDING} component={LandingPage} />
-          <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
-          <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-          <Route
-            exact
-            path={ROUTES.PASSWORD_FORGET}
-            component={PasswordForgetPage}
-          />
-          <Route exact path={ROUTES.HOME} component={HomePage} />
-          <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
-          <Route exact path={ROUTES.ADMIN} component={AdminPage} />
-        </div>
-      </Router>
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+            <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+            <Route
+              exact
+              path={ROUTES.PASSWORD_FORGET}
+              component={PasswordForgetPage}
+            />
+            <Route exact path={ROUTES.HOME} component={HomePage} />
+            <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+            <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+          </div>
+        </Router>
+      </AuthUserContext.Provider>
     );
   }
 }
