954d55158af332b42bc749d8f964dfc7c862d2e8
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-31 08:36:06 +0700

    update route and path for exact

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import { recompose } from "recompose";
+import { compose } from "recompose";
 
 import { withFirebase } from "../Firebase";
-import * as ROUTER from "../../constants/routes";
+import * as ROUTES from "../../constants/routes";
 
 const withAuthorization = condition => Component => {
   class WithAuthorization extends React.Component {
