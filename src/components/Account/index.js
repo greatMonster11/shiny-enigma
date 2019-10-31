76aa90508054aa351b97d6e4c9cfee05b3db3b4a
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-31 08:51:05 +0700

    firebase realtime database

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 
+import { withAuthorization, AuthUserContext } from "../Session";
 import { PasswordForgetForm } from "../PasswordForget";
 import PasswordChangeForm from "../PasswordChange";
-import { withAuthorization } from "../Session";
 
 const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordForgetForm />
-    <PasswordChangeForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
 const condition = authUser => !!authUser;
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,7 +14,9 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
     this.auth = app.auth();
+    this.db = app.database();
   }
 
   // *** Auth API ***
@@ -29,6 +32,12 @@ class Firebase {
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
   doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+
+  user = uid => this.db.ref(`users/${uid}`);
+
+  users = () => this.db.ref("users");
 }
 
 export default Firebase;
